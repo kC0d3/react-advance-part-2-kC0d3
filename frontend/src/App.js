3af92b00d2db6eb68238c491eb2e5605a6feb383
kC0d3 <130548527+kC0d3@users.noreply.github.com>
2023-09-04 10:58:10 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import ErrorPage from "./pages/ErrorPage";
+import Layout from "./pages/Layout";
+import About from "./pages/About";
+import Main from "./pages/Main";
+import LanguageDetails from "./pages/LanguageDetails";
+import LanguageCreator from "./pages/LanguageCreator";
+import Demo from "./pages/Demo";
+
+function App() {
+  return (
+    <></>
+    // TODO 1: implement BrowserRouter
+    
+    /* TODO 2: implement Routes */
+    
+      /* TODO 3: Implement <parent> route for Layout component */
+     
+        /* TODO 4: Implement <child> route for Main page  */
+       
+        /* TODO 10: Implement <child> route for About page */
+        
+        /* TODO 11: Implement <child> route for Demo page */
+        
+        /* TODO 12: Implement <child> route for LanguageCreator page */
+        
+        /* TODO 13: Implement <child> route for LanguageDetails page */
+        
+      /* TODO 14: Implement route for ErrorPage page */
+  )
+}
+
+export default App;
---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,25 @@
+export default function Layout() {
+  return (
+    <div>
+      <div className="navbar">
+        {/* TODO 6: Implement the <Link> component for Main page */}
+        
+          <button>Main page</button>
+        
+        {/* TODO 7: Implement the <Link> component for About page */}
+        
+          <button>About</button>
+        
+        {/* TODO 8: Implement the <Link> component for Demo page */}
+          <button>Demo</button>
+        
+        {/* TODO 9: Implement the <Link> component for LanguageCreator page */}
+          <button>Language creator</button>
+        
+      </div>
+      
+      {/* TODO 5: Implement the <Outlet> component */}
+      
+    </div>
+  );
+}
